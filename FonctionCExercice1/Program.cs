67df2e914136f6d6a89d25208fb031b552f1a66b
Charles Técher <techer.charles@educ-valadon-limoges.fr>
2022-11-14 22:01:06 +0100

    Ajoutez des fichiers projet.

---
+++ FonctionCExercice1/Program.cs
@@ -0,0 +1,58 @@
+﻿/**
+ * Exercice 1 : programme avec une fonction saisie() sans paramettrequi retourne un résultat
+ * author : CT
+ * date : 11/2021
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FonctionCExercice1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Saisie de O ou N avec contrôle de saisie
+        /// </summary>
+        /// <returns>lettre saisie</returns>
+        static char saisie()
+        {
+            char reponse;
+            do
+            {
+                Console.WriteLine();
+                Console.Write("Avez-vous un prix à saisir ? (O/N) ");
+                reponse = Console.ReadKey().KeyChar;
+            } while (reponse != 'O' && reponse != 'N');
+            return reponse;
+        }
+
+        static void Main(string[] args)
+        {
+            // Déclarations
+            float prix, total = 0;
+            char rep;
+
+            // demande si un prix est à saisir
+            rep = saisie();
+
+            // Boucle sur la saisie des prix et le cumul
+            while (rep == 'O')
+            {
+                // saisie d'un nouveau prix
+                Console.Write("   Entrez un prix = ");
+                prix = float.Parse(Console.ReadLine());
+                // cumul
+                total = total + prix;
+                // demande si un nouveau prix est à saisir
+                rep = saisie();
+            }
+
+            // Affichage du total
+            Console.WriteLine("   total des prix = " + total);
+            Console.ReadLine();
+        }
+    }
+}
